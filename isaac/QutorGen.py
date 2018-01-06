4b7cec855417ab14dfe9cbcd8e2460b95eb9c5f0
rjm49 <inbox@rjm49.dreamhosters.com>
2018-01-06 16:03:29 +0000

    extended encodings for Qutor

--- isaac/QutorGen.py
+++ isaac/QutorGen.py
@@ -1,15 +1,20 @@
+import heapq
 import os, sys
+import random
 
+import matplotlib
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.naive_bayes import BernoulliNB
+from sklearn.tree import ExtraTreeClassifier, DecisionTreeClassifier
 
-from isaac.itemencoding import gen_qenc, gen_X_primed, k_features
+from isaac.itemencoding import gen_qenc, gen_X_primed, k_features, s_features, SS_SLEV_IX
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '.'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from backfit.BackfitUtils import init_objects
 from backfit.utils.utils import DW_STRETCH, DW_LEVEL, calc_qdiff, load_new_diffs, DW_NO_WEIGHT, DW_BINARY, DW_NATTS, \
-    DW_PASSRATE, load_mcmc_diffs, DW_MCMC
+    DW_PASSRATE, load_mcmc_diffs, DW_MCMC, load_atypes
 from backfit.BackfitTest import train_and_test
 
 print(sys.path)
@@ -18,6 +23,8 @@ from sklearn.neural_network.multilayer_perceptron import MLPClassifier
 from sklearn.svm.classes import SVC, LinearSVC
 from sklearn.linear_model.logistic import LogisticRegression
 from sklearn.dummy import DummyClassifier
+from matplotlib import pyplot as plt
+matplotlib.style.use('ggplot')
 
 import pandas as pd
 import numpy
@@ -29,42 +36,50 @@ QENC_DIFF=False
 n_classes = 2
 
 FEAT_F33 = "F33"
+ROLL_LEN = 10
+PANTHEON_SIZE= 10
 
 n_users = 1000
 max_runs = None #10000
-percTest = 0.10
+percTest = 0.1
 
 predictors = [
     # DummyClassifier(strategy="stratified"),
     # DummyClassifier(strategy="uniform"),
     # BernoulliNB(),
-    #SVC(max_iter=1000, class_weight="balanced"),
-    #LinearSVC(max_iter=250, class_weight="balanced"),
-    #MLPClassifier(max_iter=250, nesterovs_momentum=True, early_stopping=True), #, activation="logistic"),
-    LogisticRegression(class_weight='balanced'),
-    # GaussianNB(),
+    # SVC(kernel='rbf', max_iter=10000, class_weight="balanced", verbose=1),
+    LinearSVC(max_iter=50),
+    MLPClassifier(max_iter=50, nesterovs_momentum=True, early_stopping=True), #, activation="logistic"),
+    # LogisticRegression(class_weight='balanced'),
+    RandomForestClassifier(class_weight="balanced"),
+     # ExtraTreeClassifier(),
+    # AdaBoostClassifier(),
+    # DecisionTreeClassifier(),
 ]
 
 predictor_params = [
     # None,
     # None,
     #{'n_iter':50, 'alpha': numpy.logspace(-3, 2) },
-    #{'n_iter':50,'C': numpy.logspace(-3, 2), 'gamma': numpy.logspace(-3, 2)},
-    #{'name':'LSVC', 'n_iter':50,'C': numpy.logspace(-3, 2)},
-    #{'name':'MLP', 'n_iter':50,'hidden_layer_sizes':[(442,),], 'learning_rate_init':[0.001, 0.01, 0.1], 'alpha': numpy.logspace(-6,2) },
-    {'name':'LOGREG', 'n_iter':50,'C': numpy.logspace(-3, 2)},
-    # None,
+    # {'name':'RBFSVC', 'n_iter':50,'C': numpy.logspace(-2, 6), 'gamma': numpy.logspace(-9, 3)},
+    {'name':'LSVC', 'n_iter':50,'C': numpy.logspace(-3, 4)},
+    {'name':'MLP', 'n_iter':50,'hidden_layer_sizes':[(300,),], 'learning_rate_init':[0.001, 0.01, 0.1], 'alpha': numpy.logspace(-6,2) },
+    # {'name':'LOGREG', 'n_iter':50,'C': numpy.logspace(-3, 2)},
+    {'name': "RFOR"},
+     # {'name':"XTREE"},
+    # {'name':"ADABoost"},
+    # {'name':"DTREE"},
 ]
 
-
-
-def generate_run_files(alpha, _featureset_to_use, _w, phi, cats, cat_lookup, all_qids, users, stretches, passrates, passquals, levels, mcmcdiffs, cat_ixs):
+def generate_run_files(alpha, _featureset_to_use, _w, phi, cats, cat_lookup, all_qids, users, stretches, passrates, passquals, levels, mcmcdf, cat_ixs, profiles=None):
     stem = _featureset_to_use+"_"+str(alpha) + "_" + str(phi) + "_" + _w
     x_filename= stem+"_X.csv"
     y_filename= stem+"_y.csv"
+    uq_filename= stem+"_uq.csv"
 
     X_file = open(stem+"_X.csv","w")
     y_file = open(stem+"_y.csv","w")
+    uq_file = open(uq_filename, "w")
 
     n_components = len(cats)
     #     all_X = numpy.zeros(shape=(0,n_features))
@@ -77,53 +92,179 @@ def generate_run_files(alpha, _featureset_to_use, _w, phi, cats, cat_lookup, all
     # print(tmx.shape[0], len(qindex))
     # assert tmx.shape[0] == len(qindex)
     # print("loaded transition data")
+    # xpcnts = pd.DataFrame.from_csv("../../isaacdata/mcmc/toplot.csv")
+    # sqmx = pd.DataFrame.from_csv("../../isaacdata/mcmc/sqmx.csv")
+    # sqmx = pd.DataFrame.from_csv("../../isaacdata/mcmc/sqmx.csv")
+    atypes = pd.DataFrame.from_csv("../../isaacdata/atypes.csv", header=None)
+    all_types = list(pd.unique(atypes[7]))
+
+    user_summary_df = pd.DataFrame(columns=["runs","age","def_lev", "start_lev", "start_lev10", "end_lev", "end_lev10","end_age", "max_lev","ts_delta","qpd","rox_delta","max_delta","pant_delta","pant_avg","rox10","roxend","pantheon"], index=users)
 
     run_ct= 0
-    X = numpy.zeros(shape=(n_components, k_features) ) #init'se a new feature vector w same width as all_X
     print("Generating files for {} users...".format(len(users)))
     for u in users:
+        u_def_lev = None
+        u_start_age = None
+        u_start_lev = None
+        u_start_10_lev = None
+        u_end_lev = None
+        u_max_lev = 0
+        u_roll_sx = []
+        u_start_ts = None
+        u_end_ts = None
+        u_pantheon = []
+        u_run_ct = 0
+        rox10=None
+        roxend=None
         print("user = ", u)
-        X[:]= 0.0
-        attempts = pd.read_csv("../../isaacdata/by_user/{}.txt".format(u), header=None)
-        runs = extract_runs_w_timestamp(attempts)
-        for run in runs:
+        S = numpy.zeros(shape=s_features)
+        X = numpy.zeros(shape=(n_components,k_features) ) #init'se a new feature vector w same width as all_X
+        oplev = 0.0
+        print(X.shape)
+        # attempts = pd.read_csv("../../isaacdata/by_user/{}.txt".format(u), header=None)
+        # runs = extract_runs_w_timestamp(attempts)
+        # fout = open("../../isaacdata/by_runs/{}.txt".format(u), "w")
+        runs = open("../../isaacdata/by_runs/{}.txt".format(u)).readlines()
+        u_run_ct = len(runs)
+        all_zero_level = True
+        for run_ix,run in enumerate(runs):
             run_ct+=1
-            ts, q, n_atts, n_pass = run
+            ts, q, n_atts, n_pass = eval(run)
+            if u_start_ts is None:
+                u_start_ts = pd.to_datetime(ts)
+            u_end_ts = pd.to_datetime(ts)
+            #fout.write(",".join(map(str,(run))) + "\n")
+            # fout.write(str(run)+"\n")
+            # continue
             qt = q.replace("|","~")
             lev = levels[qt]
-            if lev<1:
-                continue
+            if(lev!=0):
+                all_zero_level=False
+            else:
+                pass
+            print(all_types)
+            # atix = all_types.index( str( atypes.loc[qt,7] ) )
+            # print(atix)
+
+            A = numpy.zeros(shape=1)
+            A[0] = 1.0 if str(atypes.loc[qt,7])=="choice" else 0.0
+
+            # if lev<1:
+            #     continue
 
             catix = cat_ixs[ cat_lookup[qt] ]
+
+            gen=0
+            age=0
+            u_def_lev=0
+            if sprofs is not None:
+                gen = sprofs.loc[int(u),"gender"]
+                ts = pd.to_datetime(ts)
+                u_def_lev = sprofs.loc[int(u),"default_level"]
+                assert isinstance(u_def_lev, numpy.float64)
+                u_def_lev = 0 if numpy.isnan(u_def_lev) else (u_def_lev+1)
+                #print(ts)
+                dob_ts = pd.to_datetime( sprofs.loc[int(u),"date_of_birth"] )
+                #print(dob_ts)
+                age_delta = (ts - dob_ts)
+                min_age = pd.Timedelta(14*365.242, unit='d')
+                max_age = pd.Timedelta(20*365.242, unit='d')
+                age_delta = max_age if age_delta>max_age else max(age_delta, min_age)
+                age = age_delta.days / 365.242
+                if u_start_age is None:
+                    u_start_age = age_delta
+                print(age)
+                #print(age, age.seconds, age.days/365.242, type(age))
+                #exit()
+                S[0]=0 if gen=="MALE" else 1
+                S[1]=age
+                S[2]=u_def_lev
 
             passrate = passrates[qt]
             qpassqual = passquals[qt]
             stretch = stretches[qt]
-            mcmc = mcmcdiffs[qt] if qt in mcmcdiffs else 0
-
-            qenc = gen_qenc(catix, passrate, stretch, lev, qpassqual)
+            # print("PR/ST = ",passrate,stretch)
+            # sxp = xpcnts.loc[qt,"S_XP"]
+            catsxps = numpy.zeros(shape=(len(cats),))
+            # for c in cats:
+            #     cix = cat_ixs[c]
+            # catsxps[cix]=sqmx.loc[qt, c]
+            print("attempting", lev)
+            median_xp_to_s = mcmcdf.loc[qt,"mdRTS"]
+            qenc = gen_qenc(catix, median_xp_to_s, passrate, stretch, lev, qpassqual, catsxps)
 
             #X_file.write(",".join([str(x) for x in X.flatten()])+","+",".join([str(e) for e in qenc.flatten()])+"\n")
             #print(X.shape, qenc.shape)
             #print(X.flatten().shape, qenc.flatten().shape)
 
-            jnd = numpy.append(X.flatten(), qenc.flatten())
+            #jnd = numpy.append(numpy.append(X.flatten(), ">>>") , qenc.flatten())
+            # jnd = numpy.append(S.flatten(), qenc.flatten())
+            jnd = numpy.append(S.flatten(), X.flatten())
+            jnd = numpy.append(jnd, qenc.flatten())
+            jnd = numpy.append(jnd, A.flatten())
             #print(jnd.shape)
 
             X_file.write(",".join([str(j) for j in jnd]) +"\n")
 
-            X = gen_X_primed(X, catix, alpha, phi, (n_pass > 0), passrate, qpassqual, stretch) #, n_atts)
+            X, S = gen_X_primed(X, S, catix, alpha, phi, (n_pass > 0), passrate, qpassqual, stretch, lev, catsxps, n_atts)
+
+            if run_ix==0:
+                u_start_lev = lev+1
+
             if (n_pass > 0):
+                L = (lev+1)
+                u_max_lev = max(L, u_max_lev)
+                if len(u_pantheon)<PANTHEON_SIZE or L >= min(u_pantheon):
+                    heapq.heappush(u_pantheon, L)
+                    if len(u_pantheon)>PANTHEON_SIZE:
+                        heapq.heappop(u_pantheon)
+
+                u_roll_sx.append(L)
+                u_end_lev = L
+                if len(u_roll_sx)<=ROLL_LEN:
+                    u_start_10_lev = numpy.mean(u_roll_sx)
+                    rox10 = tuple(u_roll_sx)
+                else:
+                    u_roll_sx.pop(0) # roll
+                roxend = tuple(u_roll_sx)
+                u_end_10_lev = numpy.mean(u_roll_sx)
+
                 y = -1 if (n_classes == 3 and n_atts == 1) else 0
             else:
                 y = 1
 
+            uq_file.write("{},{},{}\n".format(u,qt,y))
             y_file.write(str(y)+"\n")
 
+        if(len(u_roll_sx)>9 and not all_zero_level):
+            ts_delta = (u_end_ts - u_start_ts).days
+            u_start_age = u_start_age.days / 365.242
+            u_end_age = age + (u_end_ts - u_start_ts).days / 365.242
+            if ts_delta > 7:
+                max_delta = None if (u_start_10_lev is None or u_end_10_lev is None) else (u_max_lev - u_start_10_lev)
+                rox_delta = None if (u_start_10_lev is None or u_end_10_lev is None) else (u_end_10_lev - u_start_10_lev)
+                qpd = u_run_ct / ts_delta
+                user_summary_df.loc[u,:] = [u_run_ct, u_start_age, int(u_def_lev), int(u_start_lev), u_start_10_lev, int(u_end_lev), u_end_10_lev, u_end_age, int(u_max_lev), ts_delta, qpd, rox_delta, max_delta, numpy.mean(u_pantheon)-u_start_10_lev, numpy.mean(u_pantheon), str(rox10), str(roxend), str(u_pantheon)]
+
+        # fout.close()
         X_file.flush()
         y_file.flush()
+    # x = numpy.random.randn(1000)
+    # plt.show()
+    # exit()
+
+    user_summary_df.dropna(inplace=True)
+    user_summary_df = user_summary_df.infer_objects()
+    user_summary_df.to_csv("user_summary_df.csv")
+    #deltadf = user_summary_df["rox_delta"]
+    # print(type(deltadf))
+    # print(deltadf.head())
+    # user_summary_df.loc[:,"max_lev"].plot.hist(bins=7)
+
+
     X_file.close()
     y_file.close()
+    uq_file.close()
     print(n_users, "users", run_ct,"runs", run_ct/float(n_users), "rpu")
     return x_filename,y_filename
 
@@ -135,6 +276,16 @@ if __name__ == '__main__':
         cmd = input("command please?")
     else:
         cmd = sys.argv[1]
+
+    if cmd.startswith('p'):
+        #do plots
+        user_summary_df = pd.DataFrame.from_csv("user_summary_df.csv", header=0, index_col=0)
+        print(user_summary_df.shape)
+        print(user_summary_df.dtypes)
+        print(user_summary_df.dtypes)
+        user_summary_df.iloc[:, 0:15].hist(bins="auto")
+        plt.show()
+        exit()
 
     if cmd.startswith('g'):
         do_test = False
@@ -151,37 +302,41 @@ if __name__ == '__main__':
     #users = open("../mcmc/mcmc_uesrs.txt").read().splitlines()
 
     passdiffs, stretches, passquals, all_qids = load_new_diffs("../../isaacdata/pass_diffs.csv")
-    mcmcdiffs = load_mcmc_diffs("../../isaacdata/mcmc/mcmc_results.csv")
+    mcmcdf = pd.DataFrame.from_csv("../../isaacdata/mcmc/dir_mcmc_results.csv")
+
+    sprofs = pd.DataFrame.from_csv("../../isaacdata/student_profiling/users_all.csv")
+    sprofs = sprofs[sprofs["role"]=="STUDENT"]
+    sprofs = sprofs[sprofs["date_of_birth"].notna()]
+    sprofs = sprofs[sprofs.index.isin(users)]
+    users = sprofs.index
+    print(len(users))
+
+    # adf = load_atypes("../../isaacdata/new_atypes.csv")
+    # passdiffs = adf.loc[:,"med_passrate"]
+    # stretches = adf.loc[:,"med_n_pass"] / adf.loc[:,"med_n_atts"]
 
     reports =[]
     report_name = "qutorgen_{}_{}_fb{}_opt{}_scale{}_{}.csv".format(0, n_users, str(1 if force_balanced_classes else 0), ("001" if optimise_predictors else "0"), ("1" if do_scaling else "0"), featureset_to_use)
+    conf_report = "confusion.txt"
     if do_test:
         report = open(report_name,"w")
+        conf_report = open(conf_report,"w")
     for w in [DW_BINARY]: #DW_NO_WEIGHT, DW_NATTS, DW_LEVEL, DW_PASSRATE, DW_MCMC, DW_STRETCH]:
-        for alpha in [0.25, 0.5, 0.75, 1.0]:
-            for phi_retain in [1.0, 0.75, 0.5, 0.25, 0.0]:
+        for alpha in [1.0, 0.8, 0.6, 0.4, 0.2, 0.1]: #, 0.73, 0.37, 0.1]:
+            for phi_retain in [1.0, 0.66, 0.5, 0.33, 0.0]:
                 print(cat_ixs)
                 if do_test:
                     print("testing")
                     xfn = "F33_{}_{}_{}_X.csv".format(str(alpha), str(phi_retain), w)
                     yfn = "F33_{}_{}_{}_y.csv".format(str(alpha), str(phi_retain), w)
-                    X_train, X_test, y_pred_tr, y_pred, y_true, scaler = train_and_test(alpha, predictors, predictor_params, xfn, yfn, n_users, percTest, featureset_to_use, w, phi_retain, force_balanced_classes, do_scaling, optimise_predictors, report=report)
+                    X_train, X_test, y_pred_tr, y_pred, y_true, scaler = train_and_test(alpha, predictors, predictor_params, xfn, yfn, n_users, percTest, featureset_to_use, w, phi_retain, force_balanced_classes, do_scaling, optimise_predictors, report=report, conf_report=conf_report)
                     #reports.append((alpha, report_name, y_true, y_pred))
                 else:
-                    xfn, yfn = generate_run_files(alpha, featureset_to_use, w, phi_retain, cats, cat_lookup, all_qids, users, stretches, passdiffs, passquals, levels, mcmcdiffs, cat_ixs)
+                    xfn, yfn = generate_run_files(alpha, featureset_to_use, w, phi_retain, cats, cat_lookup, all_qids, users, stretches, passdiffs, passquals, levels, mcmcdf, cat_ixs, profiles=sprofs)
                     print("gen complete, train files are",xfn,yfn)
     if do_test:
+        conf_report.close()
         report.close()
         print("complete, report file is:", report_name)
 
 
-        # wid = n_classes+1
-        # if do_test:
-        #     retains = []
-        #     f1s = []
-        #     mx = numpy.ndarray(shape=(len(reports), wid))
-        #     for ix, (alpha, r, ytr, ypd) in enumerate(reports):
-        #         mx[ix, 0] = alpha
-        #         f1s = f1_score(ytr, ypd, average=None)
-        #         mx[ix, 1:wid] = f1s
-        #     numpy.savetxt(report_name+"_to_plot.csv", mx)
--- isaac/itemencoding.py
+++ isaac/itemencoding.py
@@ -17,49 +17,93 @@ import numpy
 # F_QUAL_IX = 13
 
 n_components = 33
+s_features = 9
 q_features = 4
-k_features = 4# 10
+k_features = 3
 
-def gen_qenc(catix, passrate, stretch, lev, qpassqual):
-    qenc = numpy.zeros(shape=(n_components, q_features))
-    qenc[catix, 0]   = passrate
-    qenc[catix, 1] = stretch
-    qenc[catix, 2]   = lev
+def gen_qenc(catix, median_xp_to_s, passrate, stretch, lev, qpassqual, catsxp):
+    # qx = numpy.zeros(shape=(1,))
+    #qx[0] = numpy.sum(catsxp)
+    # qx[0] = stretch
+    qenc = numpy.zeros(shape=q_features)
+    qenc[0] = (lev+1)
+    qenc[1] = passrate
+    qenc[2] = stretch
+    qenc[3] = median_xp_to_s
     # qenc[catix, MCMC_IX] = mcmc
-    qenc[catix, 3]    = qpassqual
+    #qenc[catix, 3]    = qpassqual
+    #print(qenc.shape)
+    #print(catsxp.shape)
+    #qenc = numpy.hstack((qenc, catsxp.reshape(-1,1)))
+    #print(qenc.shape)
+    #qenc[:,3] = catsxp.reshape(33,)
+    #qenc = numpy.append(qx, qenc.flatten())
     return qenc
 
-def gen_X_primed(X, catix, alpha, fade, is_pass, passrate, qpassqual, stretch): #, n_atts):
+SS_SLEV_IX = 3
+def gen_X_primed(X, S, catix, alpha, fade, is_pass, passrate, qpassqual, stretch, lev, catsxp, n_atts):
+    #semi static traits
+    SS_OPLEV_IX = 3
+    SS_FLEV_IX = 4
+    SS_XP_IX = 5
+    SS_SXP_IX = 6
+    SS_FXP_IX = 7
+    SS_SMAX_IX = 8
 
-    RECENCY_IX=0
-    Q_CNT_IX=1
-    PRATE_IX=2
-    QUAL_IX=0
-    STRETCH_IX=0
-    SUXX_IX=0
+    LEVEL_IX=0
+    # F_LEVEL_IX=2
+    PRATE_IX=1
+    STRETCH_IX=2
     F_PRATE_IX=3
-    F_QUAL_IX=0
-    F_STRETCH_IX=0
-    FAIL_IX=0
+    F_STRETCH_IX=4
+    # RECENCY_IX =0
+    # F_RECENCY_IX=0
+    # NATTS_IX=0
+    # F_NATTS_IX=0
+    # SUXX_IX=0
+    # FAIL_IX=0
+    # XP = 0
+    # SXPAV = 0
+    # F_SXPAV = 0
 
-
-    Xc = X[catix]
-    X[:,RECENCY_IX]  =   X[:,RECENCY_IX] * fade
-    Xc[Q_CNT_IX]    +=  1.0
+    Xcat = X[catix]
+    Xcat = Xcat * fade
+    # X[:,RECENCY_IX]  =   X[:,RECENCY_IX] * fade
+    # X[:, F_RECENCY_IX] = X[:, F_RECENCY_IX] * fade
+    # X[catix, Q_CNT_IX]    +=  1.0
     #            X[catix, LEVEL_IX] += alpha*lev
     #            X[catix, MCMC_IX] += alpha*mcmc
+    # Xcat[XP] = Xcat[XP] + 1.0
+    S[SS_XP_IX] = S[SS_XP_IX] + lev+1
+    #S[SS_OPLEV_IX] = S[SS_OPLEV_IX] * (1 - alpha) + alpha * (lev+1)
     if (is_pass):
-        #Xc[PRATE_IX]    = Xc[PRATE_IX]  * (1 - alpha) + alpha * passrate
+        # print("passed",passrate, stretch, lev)
+        ix = ["PRATE_IX", "STRETCH_IX", "LEVEL_IX"]
+
+        # X[:, SXPAV] = X[:, SXPAV]*(1 - alpha) + alpha * catsxp.reshape(33,)
+        # print(X[:,SXPAV].shape)
+        S[SS_SLEV_IX] = S[SS_SLEV_IX] * (1 - alpha) + alpha * (lev+1)
+        S[SS_SXP_IX] = S[SS_SXP_IX] + lev+1
+        S[SS_SMAX_IX] = max(S[SS_SMAX_IX], (lev+1))
+        Xcat[LEVEL_IX] = Xcat[LEVEL_IX] * (1 - alpha) + alpha * (lev+1)
+        Xcat[STRETCH_IX]  = Xcat[STRETCH_IX] * (1 - alpha) + alpha * stretch
+        Xcat[PRATE_IX]    = Xcat[PRATE_IX] * (1 - alpha) + alpha * passrate
+
         #Xc[QUAL_IX]     = Xc[QUAL_IX]   * (1 - alpha) + alpha * qpassqual
-        #Xc[STRETCH_IX]  = Xc[STRETCH_IX] * (1 - alpha) + alpha * stretch
-        #Xc[NATTS_IX]    = Xc[NATTS_IX]  * (1 - alpha) + alpha * n_atts
-        #Xc[SUXX_IX]     += 1.0
-        Xc[RECENCY_IX]  = 1.0
+        # Xcat[NATTS_IX]    = Xcat[NATTS_IX]  * (1 - alpha) + alpha * n_atts
+        # Xcat[SUXX_IX]     += 1.0
+        #Xcat[RECENCY_IX]  = 1.0
     else:
-        #Xc[F_PRATE_IX]  = Xc[F_PRATE_IX]    * (1 - alpha) + alpha * passrate
+        S[SS_FXP_IX] = S[SS_FXP_IX] + lev+1
+        S[SS_FLEV_IX] = S[SS_FLEV_IX] * (1 - alpha) + alpha * (lev+1)
+        # X[:, F_SXPAV] = X[:, F_SXPAV]*(1 - alpha) + alpha * catsxp.reshape(33,)
+        #Xcat[F_PRATE_IX]  = Xcat[F_PRATE_IX] * (1 - alpha) + alpha * passrate
+        #Xcat[F_STRETCH_IX] = Xcat[F_STRETCH_IX] * (1 - alpha) + alpha * stretch
+        # Xcat[F_LEVEL_IX]  = Xcat[F_LEVEL_IX] * (1 - alpha) + alpha * lev
         #Xc[F_QUAL_IX]   = Xc[F_QUAL_IX]     * (1 - alpha) + alpha * qpassqual
-        #Xc[F_STRETCH_IX] = Xc[F_STRETCH_IX] * (1 - alpha) + alpha * stretch
-        #Xc[F_NATTS_IX]  = Xc[F_NATTS_IX]    * (1 - alpha) + alpha * n_atts
-        #Xc[FAIL_IX]     += 1.0
-        Xc[RECENCY_IX]  = -1.0
-    return X
+        # Xcat[F_NATTS_IX]  = Xcat[F_NATTS_IX]    * (1 - alpha) + alpha * n_atts
+        # Xcat[FAIL_IX]     += 1.0
+        #Xcat[F_RECENCY_IX]  = 1.0
+    X[catix] = Xcat
+    # print("X'=",X)
+    return X, S
